91cfc5c050eabe66ec45ef583cc492c8e45a7129
evanstrittmatter16 <evanstrittmatter16@stu.spusd.net>
2018-08-29 08:49:43 -0700

    added countdown timer to switch, it turn off after 5 seconds. switching off early cancels the timer

--- app/src/main/java/com/example/uselessmachine/MainActivity.java
+++ app/src/main/java/com/example/uselessmachine/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.uselessmachine;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
@@ -11,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
 
     private Button butttonSelfDestruct;
     private Switch switchUseless;
+    
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked){
+                    startSwitchOffTimer();
                     Toast.makeText(MainActivity.this, "on", Toast.LENGTH_SHORT).show();
                 } else{
                     Toast.makeText(MainActivity.this, "off", Toast.LENGTH_SHORT).show();
@@ -33,6 +38,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void startSwitchOffTimer() {
+        new CountDownTimer(5000, 100) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                if(!switchUseless.isChecked()){
+                 //   Log.d(TAG, "onTick: canceling");
+                    cancel();
+                    
+                }
+            }
+
+            @Override
+            public void onFinish() {
+            switchUseless.setChecked(false);
+               // Log.d(TAG, "onFinish: switch set to false");
+            }
+        }.start();
     }
 
     private void wireWidget() {
